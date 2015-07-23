00b5ce455847e119bcfd2a9502a1e2b1b68ccce4
421662093 <421662093@qq.com>
2015-07-23 17:38:52 +0800

    up 2015-7-23 17:38

--- app/main/views.py
+++ app/main/views.py
@@ -30,25 +30,25 @@ def index():
 
 @main.route('/apply', methods=['GET', 'POST'])
 def apply():
-
+    
     if request.method == 'POST':
         form = ExpertApplyForm()
 
         if form.submit():
-            if not ExpertApply.isphone(form.phone.data):
-                item = ExpertApply()
-                item.id = collection.get_next_id('expertapply')
-                item.name = form.name.data
-                item.phone = form.phone.data
-                item.save()
-                return '{"info":"seccess","status":"y"}'
+            if len(request.form.get('name',''))>1 and len(request.form.get('name',''))<17 and len(request.form.get('phone',''))==11:
+                if not ExpertApply.isphone(form.phone.data):
+                    item = ExpertApply()
+                    item.id = collection.get_next_id('expertapply')
+                    item.name = form.name.data
+                    item.phone = form.phone.data
+                    item.save()
+                    return '{"info":"认证信息提交成功","status":"y"}'
+                else:
+                    return '{"info":"您已提交认证信息，无需再次提交","status":"n"}'
             else:
-                return '{"info":"您已报名，无需再次提交","status":"n"}'
-            # print str(item.id) + '_' + item.name + '_' + item.phone + '_' +
-            # item.company + '_' + item.job + '_' + item.remark
-
-        else:
-            return '{"info":"error","status":"n"}'
+                return '{"info":"您所提交的姓名或手机号码格式错误，请重新填写","status":"n"}'
+        # print str(item.id) + '_' + item.name + '_' + item.phone + '_' +
+        # item.company + '_' + item.job + '_' + item.remark
     else:
         '''
         item = Event()
